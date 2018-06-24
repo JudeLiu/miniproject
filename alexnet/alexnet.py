3f2317120ed3417f0dd6278f9a9d3d4bf51567de
judeLiu <ljnsjtu@hotmail.com>
2018-06-24 18:18:37 +0800

    add different init methods

--- alexnet/alexnet.py
+++ alexnet/alexnet.py
@@ -54,30 +54,39 @@ class ConvPoolLayer(tf.keras.Model):
 
 
 class AlexNet(tf.keras.Model):
-    def __init__(self, lrn=True):
+    def __init__(self, lrn, initializer):
         super(AlexNet, self).__init__()
         # initializer = tf.variance_scaling_initializer(scale=2)
+        if initializer == 'xavier':
+            weight_initializer = tf.contrib.layers.xavier_initializer()
+        elif initializer == 'he':
+            weight_initializer = tf.keras.initializers.he_normal()
+        elif initializer == 'normal':
+            weight_initializer = tf.random_normal_initializer(mean=0, stddev=.01)
+        else:
+            raise ValueError('%s Not defined' % initializer)
+        
         first_and_second_layer = ConvLRNPoolLayer if lrn else ConvBNPoolLayer
         # self.layer1 = first_and_second_layer(filters=96, kernel_size=11, strides=4)
         self.layer1 = first_and_second_layer(filters=96, kernel_size=5, strides=1)
         self.layer2 = first_and_second_layer(filters=256, kernel_size=5, strides=1)
         self.layer3 = tf.layers.Conv2D(filters=384, kernel_size=3, strides=1, padding='same',
-                                        kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                                        kernel_initializer=weight_initializer,
                                         kernel_regularizer=tf.nn.l2_loss)
         self.layer4 = tf.layers.Conv2D(filters=384, kernel_size=3, strides=1, padding='same',
-                                        kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                                        kernel_initializer=weight_initializer,
                                         kernel_regularizer=tf.nn.l2_loss)
         self.layer5 = ConvPoolLayer(256, kernel_size=3, strides=1)
         self.flatten = tf.layers.Flatten()
         self.fc1 = tf.layers.Dense(1024, activation=tf.nn.relu,
-                                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                                    kernel_initializer=weight_initializer,
                                     kernel_regularizer=tf.nn.l2_loss)
         self.fc2 = tf.layers.Dense(1024, activation=tf.nn.relu,
-                                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                                    kernel_initializer=weight_initializer,
                                     kernel_regularizer=tf.nn.l2_loss)
         # linear output for softmax
         self.fc3 = tf.layers.Dense(10, 
-                                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                                    kernel_initializer=weight_initializer,
                                     kernel_regularizer=tf.nn.l2_loss)
 
     def __call__(self, x):
--- alexnet/train.py
+++ alexnet/train.py
@@ -4,9 +4,9 @@ import numpy as np
 import os
 import shutil
 from data_preprocess import load_data, prepare_dataset
-from alexnet import *
+from alexnet import AlexNet, AlexNetTruncated
 
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 def check_accuracy(sess, dset, X, logits):
     num_correct, num_samples = 0, 0
@@ -21,26 +21,27 @@ def check_accuracy(sess, dset, X, logits):
     return acc, num_correct, num_samples
 
 
-def build_model(X, Y, lr, **kwargs):
+def build_model(X, Y, lr, global_step, **kwargs):
     lrn = kwargs.get('lrn', False)
+    initializer = kwargs.get('init', 'he')
     full_model = kwargs.get('full_model', True)
     # build model, loss and optimizer
     model = AlexNet if full_model else AlexNetTruncated
-    logits_op = model(lrn)(X)
+    logits_op = model(lrn, initializer=initializer)(X)
     loss_op = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=Y, logits=logits_op)
     loss_op = tf.reduce_mean(loss_op, name='loss')
     correct_pred = tf.equal(tf.cast(tf.argmax(logits_op, axis=1), tf.int32), Y)
     acc_op = tf.reduce_mean(tf.cast(correct_pred, tf.float32), name='train_acc')
 
-    optim = tf.train.AdamOptimizer(lr)
-    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-    with tf.control_dependencies(update_ops):
-        train_op = optim.minimize(loss_op)
+    train_op = tf.train.AdamOptimizer(lr).minimize(loss_op, global_step=global_step)
+    # update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    # with tf.control_dependencies(update_ops):
+    #     train_op = optim.minimize(loss_op)
 
     return logits_op, loss_op, acc_op, train_op
 
 
-def create_dir(path):
+def rm_dir(path):
     if os.path.exists(path):
         try:
             shutil.rmtree(path)
@@ -49,73 +50,95 @@ def create_dir(path):
             print('Error: {} - {}.'.format(e.filename, e.strerror))
 
 def train(args):
+    if args.eval:
+        args.resume = True
+
     images, labels = load_data(True)
     train_dset, val_dset = prepare_dataset(images, labels, True, augment=args.augment)
     # images, labels = load_data(False)
     # test_dset = prepare_dataset(images, labels, False)
 
-    # learning_rate = (9+2*np.random.random(4))*1e-5 ,
-    learning_rate = [9.025036551142157e-05]
+    learning_rate = (10+1*np.random.random(10))*1e-5 ,
+    # learning_rate = [9.025036551142157e-05]
 
     for lr in learning_rate:
         tf.reset_default_graph()
         graph = tf.Graph()
-        
+
         with graph.as_default():
-            with tf.device('/device:GPU:0'):
-                if not args.resume:
-                    # input placeholder
-                    input_shape = (32, 32) if not args.augment else (24, 24)
-                    X = tf.placeholder(tf.float32, [None, *input_shape, 3], name='images_ph')
-                    Y = tf.placeholder(tf.int32, [None], name='labels_ph')
-                    logits_op, loss_op, acc_op, train_op = build_model(X, Y, lr, lrn=args.lrn, full_model=args.full_model)                                 
+            global_step = tf.Variable(0, name='global_step', trainable=False)
+            # global_step = tf.get_variable('global_step', shape=[None], dtype=tf.int32, initializer=tf.constant_initializer(0), trainable=False)
 
+            # input placeholder
+            input_shape = (32, 32) if not args.augment else (24, 24)
+            X = tf.placeholder(tf.float32, [None, *input_shape, 3], name='images_ph')
+            Y = tf.placeholder(tf.int32, [None], name='labels_ph')
+            logits_op, loss_op, acc_op, train_op = build_model(X, Y, lr, global_step, lrn=args.lrn, full_model=args.full_model)                                 
+
+            # add variables to summary
+            log_dir = os.path.join(args.log_dir, 'lr-{:.8f}'.format(lr))
             if not args.resume:
-                # add variables to summary
-                log_dir = '{}/lr-{:.8f}'.format(args.log_dir, lr)
-                create_dir(log_dir)
+                rm_dir(log_dir)
 
-                tf.summary.scalar('loss', loss_op)
-                tf.summary.scalar('train_acc', acc_op)
-                tf.summary.histogram('loss', loss_op)
-                summary_op = tf.summary.merge_all()
-                summary_writer = tf.summary.FileWriter(log_dir, graph)
+            tf.summary.scalar('loss', loss_op)
+            tf.summary.scalar('train_acc', acc_op)
+            tf.summary.histogram('loss', loss_op)
+            summary_op = tf.summary.merge_all()
+            summary_writer = tf.summary.FileWriter(log_dir, graph)
 
-                # save builder
-                save_dir = '{}/lr-{:.8f}'.format(args.save_dir, lr) if not args.resume else args.save_dir
-                create_dir(save_dir)
+            # model saver
+            save_dir = os.path.join(args.save_dir, 'lr-{:.8f}'.format(lr))  
+            saver = tf.train.Saver(filename=save_dir)
 
             # run training session
             config = tf.ConfigProto(log_device_placement=False)
             with tf.Session(graph=graph, config=config) as sess:
-                if not args.resume:
-                    sess.run(tf.global_variables_initializer())
-                else:
-                    print('Resume from {}'.format(save_dir))
-                    tf.saved_model.loader.load(sess, [tf.saved_model.tag_constants.SERVING], save_dir)
-                    X = tf.get_tensor_by_name('images_ph:0')
-                    Y = tf.get_tensor_by_name('labels_ph:0')
-                    loss_op = tf.get_tensor_by_name('')
-                print('lr={}'.format(lr)) 
-                cnt = 0
-
-                for e in range(args.epochs):
-                    print('Epoch {}'.format(e))
-                    for i, (x_batch, y_batch) in enumerate(train_dset):
-                        loss, _, acc, summary = sess.run([loss_op, train_op, acc_op, summary_op], feed_dict={X: x_batch, Y: y_batch})
-                        summary_writer.add_summary(summary, cnt)
-                        cnt += 1
-                        if i % args.print_every == 0:
-                            print('loss:{:.4f}, acc:{:.2%}'.format(loss, acc))
+                sess.run(tf.global_variables_initializer())
+                if args.resume:
+                    print('Resume from {}'.format(args.save_dir))
+                    saver.restore(sess, tf.train.latest_checkpoint('model'))
+                step = sess.run(global_step)
+                print('lr={}, normalization: {}, initializer:{}, step: {}'.format(lr, args.lrn, args.init, step)) 
+                
+                if not args.eval:
+                    for e in range(args.epochs):
+                        print('Epoch {}'.format(e))
+                        for x_batch, y_batch in train_dset:
+                            _, acc, summary = sess.run([train_op, acc_op, summary_op], feed_dict={X: x_batch, Y: y_batch})
+                            step = sess.run(global_step)
+                            summary_writer.add_summary(summary, step)
+                        saver.save(sess, save_dir, global_step=global_step)
 
                     acc, num_correct, num_samples = check_accuracy(sess, val_dset, X, logits_op)
-                    print('val acc: {:.2%} ({}/{})'.format(acc, num_correct, num_samples))
-            
-                tf.saved_model.simple_save(sess, save_dir, 
-                                    inputs={'X': X}, 
-                                    outputs={'logits': logits_op})
+                    step = sess.run(global_step)
+                    print('step:{}, val acc: {:.2%} ({}/{})'.format(step, acc, num_correct, num_samples))
+                else:
+                    images, labels = load_data(False)
+                    test_dset = prepare_dataset(images, labels, train=False, resize=args.resize)
+                    acc, num_correct, num_samples = check_accuracy(sess, test_dset, X, logits_op)
+                    step = sess.run(global_step)
+                    print('step:{}, val acc: {:.2%} ({}/{})'.format(step, acc, num_correct, num_samples))
             # with end here
 
+'''
+def evaluation(args):
+    images, labels = load_data(True)
+    train_dset, val_dset = prepare_dataset(images, labels, True, augment=args.augment)
+
+    with tf.device('/device:GPU:0'):
+        # input placeholder
+        input_shape = (32, 32) if not args.augment else (24, 24)
+        X = tf.placeholder(tf.float32, [None, *input_shape, 3], name='images_ph')
+        Y = tf.placeholder(tf.int32, [None], name='labels_ph')
+        logits_op, loss_op, acc_op, train_op = build_model(X, Y, lr, global_step, lrn=args.lrn, full_model=args.full_model)  
+
+    config = tf.ConfigProto(log_device_placement=False)
+    with tf.Session(graph=graph, config=config) as sess:
+        sess.run(tf.global_variables_initializer())
+        if args.resume:
+            print('Resume from {}'.format(args.save_dir))
+            saver.restore(sess, tf.train.latest_checkpoint('model'))
+'''
 
 def main():
     parser = argparse.ArgumentParser('Train AlexNet on CIFAR-10')
@@ -131,7 +154,11 @@ def main():
     parser.add_argument('--resume', dest='resume', action='store_true')
     parser.add_argument('--full-model', dest='full_model', action='store_true')
     parser.add_argument('--trunc-model', dest='full_model', action='store_false')
-    parser.set_defaults(lrn=False, resume=False, augment=False, full_model=True)
+    parser.add_argument('--eval', dest='eval', action='store_true')
+    parser.add_argument('--resize', dest='resize', action='store_true')
+    parser.add_argument('--init', type=str, default='he')
+    parser.set_defaults(lrn=False, resume=False, augment=False, 
+                        full_model=True, eval=False, resize=False)
     args = parser.parse_args()
     print(args)
 
@@ -139,11 +166,11 @@ def main():
         raise FileNotFoundError('Dataset does not exist in path: {}'.format(args.dataset_dir))
    
     if not os.path.exists(args.log_dir):
-        os.mkdir(args.log_dir)
+        os.makedirs(args.log_dir)
         print('Create log dir: {}'.format(args.log_dir))
 
     if not os.path.exists(args.save_dir):
-        os.mkdir(args.save_dir)
+        os.makedirs(args.save_dir)
         print('Create save dir: {}'.format(args.save_dir))
 
     train(args)
